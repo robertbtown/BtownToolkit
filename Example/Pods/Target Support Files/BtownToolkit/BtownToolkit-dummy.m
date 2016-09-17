8140e55c188fac6c96b350ff2ed87604a2ad20d0
Robert Magnusson <RobertsMacBookPro15@Roberts-MacBook-Pro.local>
2016-09-17 11:18:45 +0200

    Inital commit

---
+++ Example/Pods/Target Support Files/BtownToolkit/BtownToolkit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BtownToolkit : NSObject
+@end
+@implementation PodsDummy_BtownToolkit
+@end
---
+++ Example/Pods/Target Support Files/Pods-BtownToolkit_Example/Pods-BtownToolkit_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BtownToolkit_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BtownToolkit_ExampleVersionString[];
+
